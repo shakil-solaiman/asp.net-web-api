6b7a0671684ff0dba1f6966ae698dd948f1e822d
shakil-solaiman <solaimanshakil56@gmail.com>
2026-06-14 01:58:55 +0600

    Setup Swegger API Documentation.

--- Program.cs
+++ Program.cs
@@ -2,13 +2,18 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the Dependency Injection (DI) container
 builder.Services.AddOpenApi(); 
+builder.Services.AddEndpointsApiExplorer(); // Enables API endpoint exploration for OpenAPI/Swagger
+builder.Services.AddSwaggerGen(); // Adds Swagger generation services
+
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline
 if (app.Environment.IsDevelopment())
 {
-    app.MapOpenApi(); // Enables your API documentation in development mode
+    app.UseSwagger(); // Enable middleware to serve generated Swagger as a JSON endpoint
+    app.UseSwaggerUI(); // Enable middleware to serve Swagger UI, specifying the Swagger JSON endpoint
+    
 }
 
 app.UseHttpsRedirection();
