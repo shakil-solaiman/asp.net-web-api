e4a0289e808809c00c391e42bd51a869d4ff774c
shakil-solaiman <solaimanshakil56@gmail.com>
2026-06-15 10:25:34 +0600

    Successfully created CRUD operations using ASP.NET Core.

--- Program.cs
+++ Program.cs
@@ -20,11 +20,69 @@ app.UseHttpsRedirection();
 
 var categories = new List<Category>();
 
+// Read Categories
 
 app.MapGet("/api/categories", () =>
 {
     return Results.Ok(categories); 
 });
+
+
+// Create a category
+
+app.MapPost("/api/categories", () =>
+{
+    var newCategory = new Category
+    {
+       CategoryId = Guid.Parse("123e4567-e89b-12d3-a456-426614174000"),
+       Name = "Electronics",
+       Description = "Devices and Gadgets",
+       CreatedAt =DateTime.UtcNow,
+    };
+
+    categories.Add(newCategory);
+
+    return Results.Created($"/api/categories/{newCategory.CategoryId}",newCategory);
+});
+
+
+
+// Delete a Category
+
+app.MapDelete("/api/categories", () =>
+{
+    var foundCategory = categories.FirstOrDefault(categoryNum => categoryNum.CategoryId == Guid.Parse("123e4567-e89b-12d3-a456-426614174000"));
+
+    if(foundCategory == null)
+    {
+        return Results.NotFound("Category with this ID is not exist.");
+    }
+
+
+    categories.Remove(foundCategory);
+    return Results.NoContent();
+});
+
+
+// Update a Category
+
+app.MapPut("/api/categories", () =>
+{
+    var foundCategory = categories.FirstOrDefault(categoryNum => categoryNum.CategoryId == Guid.Parse("123e4567-e89b-12d3-a456-426614174000"));
+
+    if(foundCategory == null)
+    {
+        return Results.NotFound("Category with this ID is not exist.");
+    }
+
+    foundCategory.Name = "Smart Phone";
+    foundCategory.Description = "High Quality Products.";
+
+    return Results.NoContent();
+});
+
+
+
 
 
 
