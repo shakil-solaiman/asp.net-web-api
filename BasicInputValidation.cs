0a4c74c265f4c63e570b78e06e9e43064b79dfad
shakil-solaiman <solaimanshakil56@gmail.com>
2026-06-16 23:51:58 +0600

    Basic validation - Not null string

--- BasicInputValidation.cs
+++ BasicInputValidation.cs
@@ -1,25 +1,37 @@
-using System.Runtime.InteropServices;
-
+using System.ComponentModel.DataAnnotations;
 
 var builder = WebApplication.CreateBuilder(args);
-
-builder.Services.AddOpenApi(); 
-builder.Services.AddEndpointsApiExplorer(); 
-builder.Services.AddSwaggerGen(); 
-
-
 var app = builder.Build();
 
-if (app.Environment.IsDevelopment())
+app.MapPost("/products", (Product product) =>
 {
-    app.UseSwagger(); 
-    app.UseSwaggerUI(); 
-}
+    var validationResults = new List<ValidationResult>();
+    var validationContext = new ValidationContext(product);
 
-app.UseHttpsRedirection();
+    bool isValid = Validator.TryValidateObject(
+        product, validationContext, validationResults, validateAllProperties: true);
 
+    if (!isValid)
+    {
+        var errors = validationResults.Select(v => v.ErrorMessage);
+        return Results.BadRequest(new { Errors = errors });
+    }
 
-
-
+    return Results.Ok(new { Message = "Product is valid!", Data = product });
+});
 
 app.Run();
+
+public class Product
+{
+    [Required(ErrorMessage = "Name is required.")]
+    [StringLength(50, MinimumLength = 2, ErrorMessage = "Name must be between 2 and 50 characters.")]
+    public string Name { get; set; }
+
+    [Range(1, 10000, ErrorMessage = "Price must be between 1 and 10000.")]
+    public decimal Price { get; set; }
+
+    [Required(ErrorMessage = "Email is required.")]
+    [EmailAddress(ErrorMessage = "Invalid email format.")]
+    public string Email { get; set; }
+}
